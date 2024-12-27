eb07474d150a7d05a183fa7583d41168eb19bfb9
VSTS <kramirez27@outlook.es>
2024-12-27 13:36:58 +0100

    #[BACK] Modify Get All tipado AssetDetailsDto

---
+++ ManagamentPias.App/Features/Assets/Queries/GetAssets/AssetDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ManagamentPias.App.Features.Assets.Queries.GetAssets;
+
+public record AssetDetailsDto
+{
+    public string Description { get; init; } = null!;
+    public decimal ValuePatrimony { get; set; }
+    public decimal NumUnit { get; set; }
+    public decimal ValuationRating { get; set; }
+}
